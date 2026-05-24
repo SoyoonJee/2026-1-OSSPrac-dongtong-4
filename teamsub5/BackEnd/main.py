ba3c3f560d14e1f7634a0e073f7ba8ce1762c490
choosooa <josuah5160@dgu.ac.kr>
2026-05-24 21:49:40 +0900

    과제5

---
+++ teamsub5/BackEnd/main.py
@@ -0,0 +1,12 @@
+import time
+
+print("====================================", flush=True)
+print("A2-5-1 BackEnd Container 실행 결과", flush=True)
+print("팀번호: 4", flush=True)
+print("팀명: 동통", flush=True)
+print("BackEnd 프로그램이 정상적으로 실행되었습니다.", flush=True)
+print("Hello backend~", flush=True)
+print("====================================", flush=True)
+
+while True:
+    time.sleep(60)
